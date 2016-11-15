dbfb43d5f57ab9b33dd9caf584b79f561ddb9022
SubSeven57 <danehernandez32@gmail.com>
2016-11-15 15:07:04 -0600

    Added simple validation message.

---
+++ project7/validate.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" property="stylesheet" href="css/styles.css" type="text/css" />
+</head>
+    <body>
+      <div id="pageContainer">
+        <header>
+          <h1>Thank you!</h1>
+        </header>
+
+        <section class="content">
+          <p>
+          Thank you! <?php echo $_POST["firstname"] ?><br>
+          We will be getting in touch with you at <?php echo $_POST["email"]; ?>
+          </p>
+
+          <p>In the meantime, let us know what you think.</p>
+      </div>
+    </section>
+
+      <footer>
+        <a href ="https://www.gnu.org/licenses/lgpl-3.0.en.html">Legal</a> | <a href="https://github.com/SubSeven57/semesterProjects">Github</a>
+      </footer>
+
+    </body>
+</html>
