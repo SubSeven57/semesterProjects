4db8564a84c0499a774d3226180f6161c27670db
SubSeven57 <danehernandez32@gmail.com>
2016-11-22 16:50:32 -0600

    Saundra sucks :eyes:

--- project7/validate.php
+++ project7/validate.php
@@ -2,30 +2,30 @@
 
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thank You!</title>
-    <link rel="stylesheet" property="stylesheet" href="css/styles.css" type="text/css" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Thank You!</title>
+  <link rel="stylesheet" property="stylesheet" href="css/styles.css" type="text/css" />
 </head>
-    <body>
-      <div id="pageContainer">
-        <header>
-          <h1>Thank you!</h1>
-        </header>
+  <body>
+    <div id="pageContainer">
+      <header>
+        <h1>Thank you!</h1>
+      </header>
 
-        <section class="content">
-          <p>
-          Thank you! <?php echo $_POST["firstname"] ?><br>
-          We will be getting in touch with you at <?php echo $_POST["email"]; ?>
-          </p>
+      <section class="content">
+        <p>
+        Thank you! <?php echo $_POST["firstname"] ?><br>
+        We will be getting in touch with you at <?php echo $_POST["email"]; ?>
+        </p>
 
-          <p>In the meantime, let us know what you think.</p>
-      </div>
-    </section>
+        <p>In the meantime, let us know what you think.</p>
+    </div>
+  </section>
 
-      <footer>
-        <a href ="https://www.gnu.org/licenses/lgpl-3.0.en.html">Legal</a> | <a href="https://github.com/SubSeven57/semesterProjects">Github</a>
-      </footer>
+    <footer>
+      <a href ="https://www.gnu.org/licenses/lgpl-3.0.en.html">Legal</a> | <a href="https://github.com/SubSeven57/semesterProjects">Github</a>
+    </footer>
 
-    </body>
+  </body>
 </html>
